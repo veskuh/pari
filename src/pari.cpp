889c79ad8f8c271236cf9922acf1977208f7a7f0
Vesa-Matti Hartikainen <vesku.h@gmail.com>
2025-08-06 17:50:25 +0300

    clean pari.cpp

--- src/pari.cpp
+++ src/pari.cpp
@@ -7,23 +7,24 @@
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+    app.setOrganizationName("veskuh.net");
+    app.setOrganizationDomain("veskuh.net");
+    app.setApplicationName("Pari");
 
     QQmlApplicationEngine engine;
 
-    FileSystem *fileSystem = new FileSystem(&app);
-    
+    FileSystem *fileSystem = new FileSystem(&app);   
     engine.rootContext()->setContextProperty("fileSystem", fileSystem);
 
     Llm *llm = new Llm(&app);
     engine.rootContext()->setContextProperty("llm", static_cast<QObject*>(llm));
 
-    const QUrl url(u"qrc:/qml/pari.qml"_qs);
+    const QUrl url("qrc:/qml/pari.qml");
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
         &app, [url](QObject *obj, const QUrl &objUrl) {
             if (!obj && url == objUrl)
                 QCoreApplication::exit(-1);
         }, Qt::QueuedConnection);
     engine.load(url);
-
     return app.exec();
 }
