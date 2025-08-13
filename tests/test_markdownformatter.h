a1167837cadb68fab3fcbea714a9ee51c5fbabc0
Vesa-Matti Hartikainen <vesku.h@gmail.com>
2025-08-13 22:51:04 +0300

    vibe: add support for md list starting with dash

--- tests/test_markdownformatter.h
+++ tests/test_markdownformatter.h
@@ -22,6 +22,7 @@ private slots:
     void testCodeBlocks();
     void testMixedContent();
     void testEscapeHtml();
+    void testUnorderedListsWithDash();
 };
 
 #endif // TEST_MARKDOWNFORMATTER_H
